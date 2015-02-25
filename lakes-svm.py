54e516c24f2bdec39750f574d9ccd87c3cc38c3b
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-25 03:01:12 -0500

    a little reworking, included SVR and mean,stdev

--- lakes-svm.py
+++ lakes-svm.py
@@ -1,4 +1,5 @@
-from sklearn.svm import SVC
+from sklearn.svm import SVC, SVR
+from sklearn.metrics import mean_squared_error
 import numpy as np
 import csv
 from matplotlib import pyplot as plt
@@ -6,59 +7,79 @@ from matplotlib import pyplot as plt
 # Input file
 train_file = 'data/train_1.csv'
 test_file = 'data/test_1.csv'
-headers = ['Name','CDOM','B1','B2','B3','B4','B5','B6','B7']
+params = ['B1','B2','B3','B4','B5','B6','B7']
+subset = 'CDOM'
 
+# Find
+def find(lst, p):
+    return min([i for i, x in enumerate(lst) if x==p])
+    
 # Parse CSV
-def parse_csv(filename):
-    X = []
-    Y = []
+# args: (1) path to dataset file, and (2) list of parameters of interest
+# returns: (1) parameters 2d np.array, and (2) class, 1d np.array 
+def parse_csv(filename, params, subset):
     with open(filename, 'rb') as f:
-        reader = csv.reader(f)
+        csvreader = csv.reader(f)
+        headers = csvreader.next()
+        subset_lookup = find(headers, subset)
+        param_lookup = {}
+        for p in params:
+            param_lookup[p] = find(headers, p)
         try:
-            for row in reader:
-                print row
-                CDOM = round(float(row[1]))
-                B1 = float(row[2])
-                B2 = float(row[3])
-                B3 = float(row[4])
-                B4 = float(row[5])
-                B5 = float(row[6])
-                B6 = float(row[7])
-                B7 = float(row[8])
-                x = [B1, B2, B3, B4, B5, B6, B7]
-                y = CDOM
-                X.append(x)
-                Y.append(y)
+            X = []
+            y = []
+            for row in csvreader:
+                s = float(row[subset_lookup])
+                y.append(s)
+                sample = []
+                for p in param_lookup:
+                    i = param_lookup[p]
+                    sample.append(float(row[i]))
+                X.append(sample)
         except csv.Error as e:
             sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
-    return (X,Y)
+        except Exception as e:
+            print str(e)
+    X = np.array(X)
+    y = np.array(y)
+    return X, y
+
+# Test an SVC
+def test_svc(X1, y1, X2, y2, kernel='rbf'):
+    """
+    Uses the SVC class
+    """
+    clf = SVC(kernel='rbf', probability=True)
+    clf.fit(X1, y1)
+    err_lst = []
+    for i in range(len(y2)):
+        x = X2[i]
+        y = round(y2[i])
+        p = clf.predict(x)
+        rmse = np.sqrt(mean_squared_error([y], [p]))
+        err_lst.append(rmse)
+    return err_lst
+
+# Test an SVR
+def test_svr(X1, y1, X2, y2, kernel='rbf'):
+    """
+    Uses the SVR class
+    """
+    clf = SVR(kernel='rbf', probability=True)
+    clf.fit(X1, y1)
+    err_lst = []
+    for i in range(len(y2)):
+        x = X2[i]
+        y = round(y2[i])
+        p = clf.predict(x)
+        rmse = np.sqrt(mean_squared_error([y], [p]))
+        err_lst.append(rmse)
+    return err_lst
     
-# Convert to NP
-(X1, Y1) = parse_csv(train_file)
-(X2, Y2) = parse_csv(test_file)
-
-"""
-SVC Class
-    C=1.0
-    cache_size=200
-    class_weight=None
-    coef0=0.0
-    degree=3,
-    gamma=0.0
-    kernel='rbf'
-    max_iter=-1
-    probability=False,
-    random_state=None
-    shrinking=True
-    tol=0.001
-    verbose=False
-"""
-
-clf = SVC(kernel='rbf')
-clf.fit(X1, Y1)
-
-for i in range(len(Y2)):
-    x = X2[i]
-    y = Y2[i]
-    p = clf.predict(x)
-    print "Actual: %f\tPredicted: %f" % (y, p)
+if __name__ == '__main__':
+    (X1, y1) = parse_csv(train_file, params, subset)
+    (X2, y2) = parse_csv(test_file, params, subset)
+    c_res = test_svc(X1, y1, X2, y2)
+    r_res = test_svr(X1, y1, X2, y2)
+    print "x:%f, s:%f" % (np.mean(r_res), np.std(r_res))
+    print "x:%f, s:%f" % (np.mean(c_res), np.std(c_res))
